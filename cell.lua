6b392010c29f750175eeb9cefbc8f44ff23d9c7b
EndeyshentLabs <Themikfound@gmail.com>
2023-09-11 21:37:55 +0300

    Cell related refactor

--- cell.lua
+++ cell.lua
@@ -1,3 +1,5 @@
+Cells = {}
+
 ---@enum ContentType
 ContentType = {
 	IRON = "iron",
@@ -23,6 +25,78 @@ function Content:new(name, amount)
 	setmetatable(public, self)
 	self.__index = self
 	return public
+end
+
+CellSize = 64
+
+---Returns cell's sprite based on it's type
+---@param cell Cell
+---@return love.Image|nil
+function imageFromCell(cell)
+	local type = cell.type
+
+	if type == CellType.CONVEYOR then
+		return Images.conveyor
+	elseif type == CellType.JUNCTION then
+		return Images.junction
+	elseif type == CellType.GENERATOR then
+		return Images.generator
+	elseif type == CellType.ORE then
+		if cell.content.name == ContentType.IRON then
+			return Images.ore_iron
+		elseif cell.content.name == ContentType.GOLD then
+			return Images.ore_gold
+		end
+	end
+
+	return nil
+end
+
+---@param x integer
+---@param y integer
+---@param cell Cell
+function drawCell(x, y, cell)
+	local camX = CameraX - love.graphics.getWidth() / 2
+	local camY = CameraY - love.graphics.getHeight() / 2
+
+	if x * CellSize - CellSize >= CameraX + love.graphics.getWidth() / 2 then
+		return
+	end
+	if x * CellSize <= camX then
+		return
+	end
+	if y * CellSize - CellSize >= CameraY + love.graphics.getHeight() / 2 then
+		return
+	end
+	if y * CellSize <= camY then
+		return
+	end
+
+	local image = imageFromCell(cell)
+
+	if image then
+		local offsetX = 0
+		local offsetY = 0
+
+		if cell.direction == Direction.DOWN then
+			offsetX = CellSize
+		elseif cell.direction == Direction.LEFT then
+			offsetX = CellSize
+			offsetY = CellSize
+		elseif cell.direction == Direction.UP then
+			offsetY = CellSize
+		end
+
+		love.graphics.setColor(1, 1, 1)
+		love.graphics.draw(
+			image,
+			(x - 1) * CellSize + offsetX,
+			(y - 1) * CellSize + offsetY,
+			cell.direction * math.rad(90),
+			CellSize / 128,
+			CellSize / 128
+		)
+	end
 end
 
 ---@enum CellType
@@ -61,20 +135,147 @@ Direction = {
 ---@field direction Direction
 ---@field content Content
 ---@field under Cell|nil
+---@field update function
+---@field draw function
 Cell = {}
 
 ---Cell class constructor
+---@param x integer
+---@param y integer
 ---@param type CellType
 ---@param direction? integer
 ---@param content? Content
 ---@param under? Cell
 ---@return Cell
-function Cell:new(type, direction, content, under)
+function Cell:new(x, y, type, direction, content, under)
 	local public = {}
+	public.x = x
+	public.y = y
 	public.type = type
 	public.direction = direction or 0
 	public.content = content or Content:new()
 	public.under = under
+
+	function public:update(dt, secondPassed)
+		if self.type == CellType.GENERATOR then
+			if
+				y + 1 > CellAmount
+				or Cells[x][y + 1].type ~= CellType.CONVEYOR
+				or not self.under
+				or self.under.content.name == DEFAULT_CONTENT_NAME
+			then
+				return
+			end
+
+			if
+				(
+					Cells[x][y + 1].content.name == self.under.content.name
+					or Cells[x][y + 1].content.name == DEFAULT_CONTENT_NAME
+				) and secondPassed
+			then
+				Cells[x][y + 1].content.name = self.under.content.name
+				Cells[x][y + 1].content.amount = Cells[x][y + 1].content.amount + 1
+			end
+		elseif self.type == CellType.CONVEYOR then
+			if self.content.amount <= 0 then
+				self.content.name = DEFAULT_CONTENT_NAME
+				return
+			end
+
+			local offset = { ["x"] = 0, ["y"] = 0 }
+
+			if self.direction == Direction.RIGHT then
+				offset.x = 1
+			elseif self.direction == Direction.DOWN then
+				offset.y = 1
+			elseif self.direction == Direction.LEFT then
+				offset.x = -1
+			elseif self.direction == Direction.UP then
+				offset.y = -1
+			end
+
+			if x + offset.x <= 0 or x + offset.x > CellAmount or y + offset.y <= 0 or y + offset.y > CellAmount then
+				return
+			end
+
+			if
+				x + offset.x > CellAmount
+				or y + offset.y > CellAmount
+				or Cells[x + offset.x][y + offset.y].type ~= CellType.CONVEYOR
+				or self.content.amount == 0
+			then
+				return
+			end
+
+			if
+				(
+					Cells[x + offset.x][y + offset.y].content.name == self.content.name
+					or Cells[x + offset.x][y + offset.y].content.name == DEFAULT_CONTENT_NAME
+				) and secondPassed
+			then
+				local sub = 3
+				Cells[x + offset.x][y + offset.y].content.name = self.content.name
+
+				if self.content.amount < 3 then
+					sub = self.content.amount
+				end
+
+				assert(sub <= 3 and sub > 0, "0 < sub <= 3 (current " .. sub .. ")")
+
+				Cells[x + offset.x][y + offset.y].content.amount = Cells[x + offset.x][y + offset.y].content.amount
+					+ sub
+
+				self.content.amount = self.content.amount - sub
+
+				if self.content.amount == 0 then
+					self.content.name = DEFAULT_CONTENT_NAME
+				end
+			end
+		elseif self.type == CellType.JUNCTION then
+			if y + 1 > CellAmount or y - 1 < 0 then
+				return
+			end
+			if
+				(Cells[x][y + 1].type == CellType.CONVEYOR)
+				and (Cells[x][y - 1].type == CellType.CONVEYOR and Cells[x][y - 1].direction == Direction.DOWN)
+				and (Cells[x][y - 1].content.name == Cells[x][y + 1].content.name or Cells[x][y + 1].content.name == DEFAULT_CONTENT_NAME)
+				and (Cells[x][y - 1].content.amount > 0)
+			then
+				Cells[x][y - 1].content.amount = Cells[x][y - 1].content.amount - 1
+				Cells[x][y + 1].content.amount = Cells[x][y + 1].content.amount + 1
+				Cells[x][y + 1].content.name = Cells[x][y - 1].content.name
+			end
+
+			if x + 1 > CellAmount or x - 1 < 0 then
+				return
+			end
+			if
+				(Cells[x + 1][y].type == CellType.CONVEYOR)
+				and (Cells[x - 1][y].type == CellType.CONVEYOR and Cells[x - 1][y].direction == Direction.RIGHT)
+				and (Cells[x - 1][y].content.name == Cells[x - 1][y].content.name or Cells[x + 1][y].content.name == DEFAULT_CONTENT_NAME)
+				and (Cells[x - 1][y].content.amount > 0)
+			then
+				Cells[x - 1][y].content.amount = Cells[x - 1][y].content.amount - 1
+				Cells[x + 1][y].content.amount = Cells[x + 1][y].content.amount + 1
+				Cells[x + 1][y].content.name = Cells[x - 1][y].content.name
+			end
+		end
+	end
+
+	function public:draw()
+		if self.under then
+			drawCell(self.x, self.y, self.under)
+		end
+
+		drawCell(self.x, self.y, self)
+
+		love.graphics.setColor(0.5, 0.5, 0.5)
+		love.graphics.print(
+			self.content.name .. "\n" .. self.content.amount,
+			(self.x - 1) * CellSize + 1,
+			(self.y - 1) * CellSize + 1
+		)
+	end
 
 	setmetatable(public, self)
 	self.__index = self
--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ require("cell")
 require("ui")
 local camera = require("lib.hump.camera")
 
-local cellAmount = 20
+CellAmount = 20
 
 ---@type Direction
 Rotation = Direction.RIGHT
@@ -31,18 +31,18 @@ local function generateMap()
 	local baseX = 10000 * love.math.random()
 	local baseY = 10000 * love.math.random()
 
-	for x = 1, cellAmount do
+	for x = 1, CellAmount do
 		oreGrid[x] = {}
 
-		for y = 1, cellAmount do
+		for y = 1, CellAmount do
 			oreGrid[x][y] = love.math.noise(baseX + 0.1 * x, baseY + 0.1 * y)
 		end
 	end
 
-	for x = 1, cellAmount, 1 do
+	for x = 1, CellAmount, 1 do
 		Cells[x] = {}
 
-		for y = 1, cellAmount, 1 do
+		for y = 1, CellAmount, 1 do
 			local type = 0
 			local contentName = DEFAULT_CONTENT_NAME
 
@@ -56,10 +56,14 @@ local function generateMap()
 				end
 			end
 
-			Cells[x][y] = Cell:new(type, nil, Content:new(contentName))
+			Cells[x][y] = Cell:new(x, y, type, nil, Content:new(contentName))
 		end
 	end
 end
+
+Camera = nil
+CameraX = 0
+CameraY = 0
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function love.load()
@@ -70,7 +74,6 @@ function love.load()
 	end
 
 	Camera = camera()
-	Cells = {}
 	Font = love.graphics.newFont("res/fonts/fira.ttf", 15)
 
 	Images = {}
@@ -94,9 +97,6 @@ function love.load()
 	dumpMap()
 end
 
-local cameraX = 0
-local cameraY = 0
-
 local time = 1
 
 ---@diagnostic disable-next-line: duplicate-set-field
@@ -115,199 +115,22 @@ function love.update(dt)
 	end
 
 	if love.keyboard.isDown("w") then
-		cameraY = cameraY - 300 * dt
+		CameraY = CameraY - 300 * dt
 	elseif love.keyboard.isDown("s") then
-		cameraY = cameraY + 300 * dt
+		CameraY = CameraY + 300 * dt
 	end
 	if love.keyboard.isDown("a") then
-		cameraX = cameraX - 300 * dt
+		CameraX = CameraX - 300 * dt
 	elseif love.keyboard.isDown("d") then
-		cameraX = cameraX + 300 * dt
+		CameraX = CameraX + 300 * dt
 	end
 
-	Camera:lookAt(cameraX, cameraY)
+	Camera:lookAt(CameraX, CameraY)
 
 	for x, _ in pairs(Cells) do
-		for y, cell in pairs(Cells[x]) do
-			local type = cell.type
-
-			if type == CellType.GENERATOR then
-				if
-					y + 1 > cellAmount
-					or Cells[x][y + 1].type ~= CellType.CONVEYOR
-					or not cell.under
-					or Cells[x][y].under.content.name == DEFAULT_CONTENT_NAME
-				then
-					goto continue
-				end
-
-				if
-					(
-						Cells[x][y + 1].content.name == cell.under.content.name
-						or Cells[x][y + 1].content.name == DEFAULT_CONTENT_NAME
-					) and secondPassed
-				then
-					Cells[x][y + 1].content.name = cell.under.content.name
-					Cells[x][y + 1].content.amount = Cells[x][y + 1].content.amount + 1
-				end
-			elseif type == CellType.CONVEYOR then
-				if cell.content.amount <= 0 then
-					Cells[x][y].content.name = DEFAULT_CONTENT_NAME
-					goto continue
-				end
-
-				local offset = { ["x"] = 0, ["y"] = 0 }
-
-				if cell.direction == Direction.RIGHT then
-					offset.x = 1
-				elseif cell.direction == Direction.DOWN then
-					offset.y = 1
-				elseif cell.direction == Direction.LEFT then
-					offset.x = -1
-				elseif cell.direction == Direction.UP then
-					offset.y = -1
-				end
-
-				if x + offset.x <= 0 or x + offset.x > cellAmount or y + offset.y <= 0 or y + offset.y > cellAmount then
-					goto continue
-				end
-
-				if
-					x + offset.x > cellAmount
-					or y + offset.y > cellAmount
-					or Cells[x + offset.x][y + offset.y].type ~= CellType.CONVEYOR
-					or Cells[x][y].content.amount == 0
-				then
-					goto continue
-				end
-
-				if
-					(
-						Cells[x + offset.x][y + offset.y].content.name == cell.content.name
-						or Cells[x + offset.x][y + offset.y].content.name == DEFAULT_CONTENT_NAME
-					) and secondPassed
-				then
-					local sub = 3
-					Cells[x + offset.x][y + offset.y].content.name = cell.content.name
-
-					if Cells[x][y].content.amount < 3 then
-						sub = Cells[x][y].content.amount
-					end
-
-					assert(sub <= 3 and sub > 0, "0 < sub <= 3 (current " .. sub .. ")")
-
-					Cells[x + offset.x][y + offset.y].content.amount = Cells[x + offset.x][y + offset.y].content.amount
-						+ sub
-
-					Cells[x][y].content.amount = Cells[x][y].content.amount - sub
-
-					if Cells[x][y].content.amount == 0 then
-						Cells[x][y].content.name = DEFAULT_CONTENT_NAME
-					end
-				end
-			elseif type == CellType.JUNCTION then
-				if y + 1 > cellAmount or y - 1 < 0 then
-					goto continue
-				end
-				if
-					(Cells[x][y + 1].type == CellType.CONVEYOR)
-					and (Cells[x][y - 1].type == CellType.CONVEYOR and Cells[x][y - 1].direction == Direction.DOWN)
-					and (Cells[x][y - 1].content.name == Cells[x][y + 1].content.name or Cells[x][y + 1].content.name == DEFAULT_CONTENT_NAME)
-					and (Cells[x][y - 1].content.amount > 0)
-				then
-					Cells[x][y - 1].content.amount = Cells[x][y - 1].content.amount - 1
-					Cells[x][y + 1].content.amount = Cells[x][y + 1].content.amount + 1
-					Cells[x][y + 1].content.name = Cells[x][y - 1].content.name
-				end
-
-				if x + 1 > cellAmount or x - 1 < 0 then
-					goto continue
-				end
-				if
-					(Cells[x + 1][y].type == CellType.CONVEYOR)
-					and (Cells[x - 1][y].type == CellType.CONVEYOR and Cells[x - 1][y].direction == Direction.RIGHT)
-					and (Cells[x - 1][y].content.name == Cells[x - 1][y].content.name or Cells[x + 1][y].content.name == DEFAULT_CONTENT_NAME)
-					and (Cells[x - 1][y].content.amount > 0)
-				then
-					Cells[x - 1][y].content.amount = Cells[x - 1][y].content.amount - 1
-					Cells[x + 1][y].content.amount = Cells[x + 1][y].content.amount + 1
-					Cells[x + 1][y].content.name = Cells[x - 1][y].content.name
-				end
-			end
-			::continue::
+		for _, cell in pairs(Cells[x]) do
+			cell:update(dt, secondPassed)
 		end
-	end
-end
-
-local spacing = 64
-
----Returns cell's sprite based on it's type
----@param cell Cell
----@return love.Image|nil
-local function imageFromCell(cell)
-	local type = cell.type
-
-	if type == CellType.CONVEYOR then
-		return Images.conveyor
-	elseif type == CellType.JUNCTION then
-		return Images.junction
-	elseif type == CellType.GENERATOR then
-		return Images.generator
-	elseif type == CellType.ORE then
-		if cell.content.name == ContentType.IRON then
-			return Images.ore_iron
-		elseif cell.content.name == ContentType.GOLD then
-			return Images.ore_gold
-		end
-	end
-
-	return nil
-end
-
----@param x integer
----@param y integer
----@param cell Cell
-local function drawCell(x, y, cell)
-	local camX = cameraX - love.graphics.getWidth() / 2
-	local camY = cameraY - love.graphics.getHeight() / 2
-
-	if x * spacing - spacing >= cameraX + love.graphics.getWidth() / 2 then
-		return
-	end
-	if x * spacing <= camX then
-		return
-	end
-	if y * spacing - spacing >= cameraY + love.graphics.getHeight() / 2 then
-		return
-	end
-	if y * spacing <= camY then
-		return
-	end
-
-	local image = imageFromCell(cell)
-
-	if image then
-		local offsetX = 0
-		local offsetY = 0
-
-		if cell.direction == Direction.DOWN then
-			offsetX = spacing
-		elseif cell.direction == Direction.LEFT then
-			offsetX = spacing
-			offsetY = spacing
-		elseif cell.direction == Direction.UP then
-			offsetY = spacing
-		end
-
-		love.graphics.setColor(1, 1, 1)
-		love.graphics.draw(
-			image,
-			(x - 1) * spacing + offsetX,
-			(y - 1) * spacing + offsetY,
-			cell.direction * math.rad(90),
-			spacing / 128,
-			spacing / 128
-		)
 	end
 end
 
@@ -317,54 +140,43 @@ function love.draw()
 
 	Camera:attach()
 
-	local a = math.ceil((love.mouse.getX() - (love.graphics.getWidth() / 2) + cameraX) / spacing)
-	local b = math.ceil((love.mouse.getY() - (love.graphics.getHeight() / 2) + cameraY) / spacing)
+	local a = math.ceil((love.mouse.getX() - (love.graphics.getWidth() / 2) + CameraX) / CellSize)
+	local b = math.ceil((love.mouse.getY() - (love.graphics.getHeight() / 2) + CameraY) / CellSize)
 
-	local previewImage = imageFromCell(Cell:new(BuildSelection))
+	local previewImage = imageFromCell(Cell:new(0, 0, BuildSelection))
 
 	if previewImage then
 		local previewOffsetX = 0
 		local previewOffsetY = 0
 
 		if Rotation == 1 then
-			previewOffsetX = spacing
+			previewOffsetX = CellSize
 		elseif Rotation == 2 then
-			previewOffsetX = spacing
-			previewOffsetY = spacing
+			previewOffsetX = CellSize
+			previewOffsetY = CellSize
 		elseif Rotation == 3 then
-			previewOffsetY = spacing
+			previewOffsetY = CellSize
 		end
 
 		love.graphics.setColor(1, 1, 1, 0.5)
 		love.graphics.draw(
 			previewImage,
-			(a - 1) * spacing + previewOffsetX,
-			(b - 1) * spacing + previewOffsetY,
+			(a - 1) * CellSize + previewOffsetX,
+			(b - 1) * CellSize + previewOffsetY,
 			Rotation * math.rad(90),
-			spacing / 128,
-			spacing / 128
+			CellSize / 128,
+			CellSize / 128
 		)
 	end
 
 	-- Draw grid
 	for x, _ in pairs(Cells) do
 		for y, cell in pairs(Cells[x]) do
-			if cell.under then
-				drawCell(x, y, cell.under)
-			end
-
-			drawCell(x, y, cell)
-
-			love.graphics.setColor(0.5, 0.5, 0.5)
-			love.graphics.print(
-				cell.content.name .. "\n" .. cell.content.amount,
-				(x - 1) * spacing + 1,
-				(y - 1) * spacing + 1
-			)
+			cell:draw()
 
 			love.graphics.setColor(0.1, 0.1, 0.1)
-			love.graphics.line(x * spacing, y, x * spacing, love.graphics.getHeight())
-			love.graphics.line(x, y * spacing, love.graphics.getWidth(), y * spacing)
+			love.graphics.line(x * CellSize, y, x * CellSize, love.graphics.getHeight())
+			love.graphics.line(x, y * CellSize, love.graphics.getWidth(), y * CellSize)
 		end
 	end
 
@@ -400,18 +212,19 @@ function love.mousepressed(mouseX, mouseY, button)
 		return
 	end
 
-	local x = mouseX - love.graphics.getWidth() / 2 + cameraX
-	local y = mouseY - love.graphics.getHeight() / 2 + cameraY
-	local a = math.ceil(x / spacing)
-	local b = math.ceil(y / spacing)
+	local x = mouseX - love.graphics.getWidth() / 2 + CameraX
+	local y = mouseY - love.graphics.getHeight() / 2 + CameraY
+	local a = math.ceil(x / CellSize)
+	local b = math.ceil(y / CellSize)
 
-	if a > cellAmount or b > cellAmount or a <= 0 or b <= 0 or mouseY <= 48 then
+	if a > CellAmount or b > CellAmount or a <= 0 or b <= 0 or mouseY <= 48 then
 		return
 	end
 
 	print(("Mouse %d: %d, %d"):format(button, a, b))
 
-	local under = Cell:new(Cells[a][b].type, nil, Content:new(Cells[a][b].content.name, Cells[a][b].content.amount))
+	local under =
+		Cell:new(a, b, Cells[a][b].type, nil, Content:new(Cells[a][b].content.name, Cells[a][b].content.amount))
 	local iserase = button == 2
 
 	if not iserase then
