4b3983406cca1219ccd070470bcf18f888d0806f
EndeyshentLabs <Themikfound@gmail.com>
2023-11-25 16:19:33 +0300

    [vudu] Fix calling convention. Enable default hotkeys

--- main.lua
+++ main.lua
@@ -83,7 +83,8 @@ function love.load()
 	if isDebug() then
 		---@diagnostic disable-next-line: lowercase-global
 		vudu = require("lib.vudu.vudu")
-		vudu:initialize()
+		vudu.initialize()
+		vudu.initializeDefaultHotkeys()
 	end
 
 	for _, v in pairs(arg) do
