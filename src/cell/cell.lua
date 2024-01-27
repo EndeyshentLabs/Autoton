6a029a6035b8a5636ff997059e973515afdcf7d8
EndeyshentLabs <Themikfound@gmail.com>
2024-01-27 13:15:04 +0300

    [cell.draw/alt view] Don't draw if no image for storage's content

--- src/cell/cell.lua
+++ src/cell/cell.lua
@@ -9,12 +9,14 @@ CellType = {
 		buildable = false,
 		isStorage = false,
 		drawable = false,
+		_BASED_NAME = "NONE"
 	},
 	ORE = {
 		displayName = "ORE",
 		buildable = false,
 		isStorage = false,
 		drawable = false,
+		_BASED_NAME = "ORE"
 	},
 }
 
@@ -238,8 +240,10 @@ function DrawCell(cell)
 		if cell.type.isStorage then
 			-- TODO: Make storage an array of `Content`s
 			for k, _ in pairs(cell.storage) do
-				love.graphics.draw(k.image, (cell.x - 1) * CellSize + CellSize / 2, (cell.y - 1) * CellSize, 0, CellSize / 256, CellSize / 256)
-				break -- yes, we're just doing 1 iteration
+				if k and k.image then
+					love.graphics.draw(k.image, (cell.x - 1) * CellSize + CellSize / 2, (cell.y - 1) * CellSize, 0, CellSize / 256, CellSize / 256)
+					break -- yes, we're just doing 1 iteration
+				end
 			end
 		end
 	end
