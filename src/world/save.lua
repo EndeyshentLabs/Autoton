41d6d1114c694a873aea0969c9bc1f1fcbb8369e
EndeyshentLabs <Themikfound@gmail.com>
2024-01-27 14:42:19 +0300

    [world/save] Fix `UnwrapCell` and `CellToString`. Fixes saving and loading

--- src/world/save.lua
+++ src/world/save.lua
@@ -1,61 +1,56 @@
----comment
----@param cell Cell
+---Unwraps `cell`s fileds
+---@param cell any
 ---@return integer|nil
 ---@return integer|nil
----@return integer|nil
----@return integer|nil
+---@return CellOpts
+---@return Direction
 ---@return Content|nil
 ---@return Cell|nil
 function UnwrapCell(cell)
 	if cell == nil then
-		return nil, nil, nil, nil, nil, nil
-	end
-	local x, y, type, direction, content, under
-
-	for k, v in pairs(cell) do
-		if k == "x" then
-			x = v
-		elseif k == "y" then
-			y = v
-		elseif k == "type" then
-			type = v
-		elseif k == "direction" then
-			direction = v
-		elseif k == "content" then
-			content = { ["name"] = v.name or "FIX_ME", ["amount"] = v.amount }
-		elseif k == "under" then
-			local _x, _y, _type, _direction, _content = UnwrapCell(v)
-			under = Cell:new(
-				_x or 0,
-				_y or 0,
-				_type or 0,
-				_direction or 0,
-				_content or { ["name"] = "BUG", ["amount"] = -11037 }
-			)
-		end
+		return nil, nil, CellType["NONE"], 0, nil, nil
 	end
 
-	return x, y, type, direction, content, under
+	local x = cell.x
+	local y = cell.y
+	local _type = cell.type
+	local direction = cell.direction
+	local content = cell.content
+	local under
+
+	if cell.under then
+		local _x, _y, __type, _direction, _content = UnwrapCell(cell.under)
+		under = Cell:new(
+			_x or 0,
+			_y or 0,
+			__type,
+			_direction or 0,
+			_content
+		)
+	end
+
+	return x, y, _type, direction, content, under
 end
 
+---Converts cell to valid lua Cell contrusction string
+---@param cell Cell
+---@return string|nil
 function CellToString(cell)
 	if not cell then
 		return
 	end
-	local x, y, type, direction, content, under = UnwrapCell(cell)
-	local us = ""
+	local x, y, _type, direction, content, under = UnwrapCell(cell)
+	local us = "nil"
 	if under then
 		us = CellToString(under)
 	end
-	if us == "" then
-		us = "nil"
-	end
-	return ("Cell:new(%d, %d, %d, %d, %s, %s)"):format(
+	return ('Cell:new(%d, %d, CellType["%s"], %d, Content:new(ContentType["%s"], %d), %s)'):format(
 		x,
 		y,
-		type,
+		_type._BASED_NAME,
 		direction,
-		('{ ["name"] = "%s", ["amount"] = %d }'):format(content.name or "FIX_ME", content.amount or 0),
-		us
+		content.opts._BASED_NAME,
+		content.amount,
+		us or "nil"
 	)
 end
