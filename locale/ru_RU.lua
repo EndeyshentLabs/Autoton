0e12b649a42ec3292ae638ae80f206465b984f70
EndeyshentLabs <Themikfound@gmail.com>
2024-04-01 17:15:39 +0300

    Add experimental Russian locale

---
+++ locale/ru_RU.lua
@@ -0,0 +1,15 @@
+local locale = {
+	oreIron = "Железная руда",
+	oreGold = "Золотая руда",
+	cellMinerName = "Бур",
+	cellMinerDesc = "Добывает ресурсы",
+	cellConveyorName = "Конвейер",
+	cellConveyorDesc = ">Передвигает> ресурсы",
+	cellJunctionName = "Перекрёсток",
+	cellJunctionDesc = "Работает по принципу -|-",
+	cellStorageName = "Хранилище",
+	cellStorageDesc = "Хранилище для 192-х предметов",
+	cellCoreName = "Ядро",
+}
+
+return locale
